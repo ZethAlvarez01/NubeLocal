dcf7ff812ab273a1e824c7b64fec62b1cab439e7
ZethAlvarez01 <zethalvarezh@hotmail.com>
2020-10-06 02:03:08 -0500

    Listo get de arbol de archivos. Agregados try-catch.

--- routes/route.js
+++ routes/route.js
@@ -11,11 +11,41 @@ var rimraf = require("rimraf");
 const dirTree = require("directory-tree");
 const { log } = require('console');
 
-router.get('/', (req,res) =>{
+//Get de rutas y archivos
+router.get('/:path?',(req,res) =>{
     let dirPath = path.join(__dirname,'../public/uploads');
-    const tree = dirTree(dirPath);
-    console.log(tree);
-    res.json(tree);
+    let carpetas = [];
+
+    if(req.params.path !== undefined){
+        let dir = splitPath(req.params.path);
+        dirPath = path.join(dirPath,dir[0]);
+        carpetas = dir[1];
+    }
+
+    if (fs.existsSync(dirPath)) {
+        const tree = dirTree(dirPath);
+        console.log(tree);
+    
+        let element = [];
+        for(let i=0;i<tree.children.length;i++){
+            element.push(tree.children[i].type);
+        }
+    
+        res.json({
+            message: 'success',
+            success: false,
+            path: dirPath,
+            elements: element
+        });
+
+    }else{
+        res.status(400).json({
+            message: 'Doesnt exist',
+            success: false,
+            path: dirPath
+        });
+    }
+    
 });
 
 router.get('/adios', (req,res) =>{
@@ -43,9 +73,17 @@ router.delete('/dir/:path', async (req,res)=>{
 
     if (fs.existsSync(dirPath)) {
         console.log(dirPath2);
-        const file = await File.remove({ "path": {$regex: "^"+dirPath2}});
-        console.log(file);
-    
+        try{
+            const file = await File.remove({ "path": {$regex: "^"+dirPath2}});    
+            console.log(file);
+        }catch(err){
+            return res.status(400).json({
+                message: 'No se pudo borrar',
+                success: false,
+                error: err
+            });
+        }
+
         rimraf(dirPath, function () { 
             console.log('done'); 
         }); 
@@ -71,7 +109,6 @@ router.delete('/dir/:path', async (req,res)=>{
 //Borra archivos
 router.delete('/file/:id/:path?',async (req,res)=>{
     let dirPath = path.join(__dirname,'../public/uploads');
-    let createPath = dirPath;
     let carpetas = [];
 
     if(req.params.path !== undefined){
@@ -81,7 +118,15 @@ router.delete('/file/:id/:path?',async (req,res)=>{
     }
     if (fs.existsSync(dirPath)) {
         const { id } = req.params;
+        try{
         const file = await File.findByIdAndDelete(id);
+        }catch(err){
+            return res.status(400).json({
+                message: 'Doesnt exist file',
+                success: false,
+                error: err
+            });
+        }
         console.log(file);
         await unlink(path.join(dirPath,file.name));
         return res.json({
@@ -113,9 +158,7 @@ router.post('/dir/:path',(req,res)=>{
 
     console.log(carpetas);
 
-        if (fs.existsSync(dirPath)) {
-            console.log("Existe");
-        }else{
+        if (!fs.existsSync(dirPath)) {
             console.log("No existe");
 
             for(let i=0;i<carpetas.length;i++){
@@ -160,9 +203,7 @@ router.post('/:path?', async (req,res) =>{
 
         console.log(carpetas);
 
-        if (fs.existsSync(dirPath)) {
-            console.log("Existe");
-        }else{
+        if (!fs.existsSync(dirPath)) {
             console.log("No existe");
 
             for(let i=0;i<carpetas.length;i++){
@@ -183,23 +224,34 @@ router.post('/:path?', async (req,res) =>{
             file.md5 = f[i].md5;
             file.created_at = new Date(Date.now());
 
-            await file.save();
+            try{
+                await file.save();
+            }catch(err){
+                return res.status(400).json({
+                    message: 'No se pudo guardar',
+                    success: false,
+                    error: err
+                });
+            }
+            
 
             // Mover el archivo a la carpeta 
             f[i].mv(path.join(dirPath,f[i].name),(err)=>{
                 if (err) {
-                    console.log("Error al mover "+err);
+                    return res.status(400).json({
+                        message: 'No se pudo guardar',
+                        success: false,
+                        error: err
+                    });
                 } else {
-                    console.log("Subido");
+                    return res.json({ 
+                        message: 'File received',
+                        success: true,
+                        path: dirPath
+                    });
                 }
             });
         }
-
-        return res.json({ 
-            message: 'File received',
-            success: true,
-            path: dirPath
-        });
     }
     
 });
